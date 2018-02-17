5f5df395d19ce071890a7bcc02b469afc968f5d3
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-02-17 13:31:32 +0000

    similar-to-arrays-but-access-them-through-properties

---
+++ build-javascript-objects.js
@@ -0,0 +1,20 @@
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Danny",
+  "legs": 4,
+  "tails": 1,
+  "friends":["People"]
+  
+  
+  
+  
+};
