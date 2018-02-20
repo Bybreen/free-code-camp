1f6c9d97da69fd72e5b64e2c1dc914b3a45cb066
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-02-20 07:57:05 +0000

    use-json-to-store-data

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Stevie Wonder",
+    "title": "Songs In The Key Of Life",
+    "release_year": 1976,
+    "formats": [
+      "CS",
+      "8T",
+      "LP" ],
+    "gold": true
+  },
+];
+
