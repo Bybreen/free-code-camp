a39f8c733db249d95d3fc8916545373e78c66a0b
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-02-24 07:08:20 +0000

    use-a-while-loop-to-resolve

---
+++ generate-random-fraction-with-javascript.js
@@ -0,0 +1,11 @@
+function randomFraction() {
+
+  // Only change code below this line.
+var answer = 0;
+  while (answer === 0) {
+    answer = Math.random();
+  }
+  return answer;
+
+  // Only change code above this line.
+}
