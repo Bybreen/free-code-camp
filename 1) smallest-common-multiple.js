9b8c6460ff43c946412565ffee09761e58c91467
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-05-30 08:47:26 +0100

    On the way to solving
    The aim is to find the lowest number  of two multiples

---
+++ 1) smallest-common-multiple.js
@@ -0,0 +1,15 @@
+function smallestCommons(arr) {
+  var range =arr.sort();
+  //sort the array
+  var array = [""];
+  // create empty array
+  for (var i= arr[0]; i<= arr[arr.length-1]; i++){
+    array.push(i);
+    // adds to end of array
+  }
+  var x = true;
+  return arr;
+}
+
+
+smallestCommons([5,1]);
